5791cc7e8354edeae3fe7617da821cc32b41fef0
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2025-10-05 21:57:54 +0530

    uhbj

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -32,40 +32,58 @@ function Login() {
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}>
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "300px",
-          gap: "15px",
-          border: "1px solid gray",
-          padding: "20px",
-          borderRadius: "10px",
-        }}
-      >
-        <h2>Login</h2>
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Login</button>
-        {message && <p>{message}</p>}
-      </form>
-    </div>
+
+<div className="">
+ 
+<div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+   <h1  className="text-3xl font-bold flex justify-center items-center  text-blue-700 mb-8">
+    Welcome to <span className="text-gray-800 ">theBookStore !</span>
+  </h1>
+  <form
+    onSubmit={handleSubmit}
+    className="flex flex-col w-80 gap-4 border border-gray-300 p-6 rounded-xl bg-white shadow-lg"
+  >
+    <h2 className="text-2xl font-semibold text-center text-gray-700 mb-2">
+      Login
+    </h2>
+
+    <input
+      type="email"
+      name="email"
+      placeholder="Enter email"
+      value={formData.email}
+      onChange={handleChange}
+      required
+      className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+    />
+
+    <input
+      type="password"
+      name="password"
+      placeholder="Enter password"
+      value={formData.password}
+      onChange={handleChange}
+      required
+      className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+    />
+
+    <button
+      type="submit"
+      className="py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300 text-lg font-medium"
+    >
+      Login
+    </button>
+
+    {message && (
+      <p className="text-center text-red-500 mt-2 text-sm">{message}</p>
+    )}
+  </form>
+</div>
+
+</div>
+
+
+
   );
 }
 
